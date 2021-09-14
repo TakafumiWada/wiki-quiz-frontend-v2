51aaf4abc59456353ec7438107c8763cd669fba7
WadaTakafumi <wdtk2525@gmail.com>
2021-09-14 19:33:20 +0900

    ADD: SNSのURLを設定

---
+++ src/config.ts
@@ -0,0 +1,3 @@
+export const TWITTER_URL = "https://twitter.com/wdtk2525";
+export const NOTE_URL = "https://note.com/wadeee";
+export const FACEBOOK_URL = "https://www.facebook.com/takafumi.wada.528";
