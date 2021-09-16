7d96074d1a6af6b131ddbe1643ce7eff019d0a2b
WadaTakafumi <wdtk2525@gmail.com>
2021-09-16 18:21:24 +0900

    ADD: 変数をconfigに切り出し

---
+++ src/config/index.ts
@@ -0,0 +1,6 @@
+export const TWITTER_URL = "https://twitter.com/wdtk2525";
+export const NOTE_URL = "https://note.com/wadeee";
+export const FACEBOOK_URL = "https://www.facebook.com/takafumi.wada.528";
+
+export const SELECTED_CATEGORIES_NUMBER = 3;
+export const SELECTED_WORDS_NUMBER = 9;
