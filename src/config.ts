3e83ad4ec90f979fc1122b0d6321d8634f3dbca4
WadaTakafumi <wdtk2525@gmail.com>
2021-09-16 18:22:28 +0900

    微調整

--- src/config.ts
+++
@@ -1,3 +0,0 @@
-export const TWITTER_URL = "https://twitter.com/wdtk2525";
-export const NOTE_URL = "https://note.com/wadeee";
-export const FACEBOOK_URL = "https://www.facebook.com/takafumi.wada.528";
