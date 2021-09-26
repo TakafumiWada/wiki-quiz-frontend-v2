8ae57e9966d11bee6bed2ddcf91daaa00ce2ae7d
WadaTakafumi <wdtk2525@gmail.com>
2021-09-26 09:23:11 +0900

    FIX: stateを分割

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,32 +1,15 @@
 import { InjectionKey } from "vue";
 import { createStore, useStore as baseUseStore, Store } from "vuex";
 
-import { articleApi } from "@/api/article";
-import { ArticleState, MutationTypes, ActionTypes } from "./types";
-import { SELECTED_CATEGORIES_NUMBER, SELECTED_WORDS_NUMBER } from "@/config";
+import { ArticleState } from "./types";
+import { state } from "./state";
 import { mutations } from "./mutations";
 import { actions } from "./actions";
 
 export const key: InjectionKey<Store<ArticleState>> = Symbol();
 
 export default createStore<ArticleState>({
-  state: {
-    article: {
-      categories: [],
-      contents: [],
-      title: "",
-      topic: {},
-      words: [],
-      image: "",
-      url: "",
-    },
-    searchResult: "",
-    selectedCategoriesNumber: SELECTED_CATEGORIES_NUMBER,
-    selectedCategories: [],
-    selectedWordsNumber: SELECTED_WORDS_NUMBER,
-    selectedWords: [],
-    isLoading: false,
-  },
+  state,
   mutations,
   actions,
 });
