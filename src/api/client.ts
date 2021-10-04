df09c70d56c50756ccbeca8fdbe1ca2b7fa8725f
WadaTakafumi <wdtk2525@gmail.com>
2021-10-05 07:16:49 +0900

    UPDATE: 仕様書通りのAPIを実装

--- src/api/client.ts
+++
@@ -1,6 +0,0 @@
-const API_BASE_URL = process.env.VUE_APP_API_URL;
-import axios, { AxiosInstance } from "axios";
-
-export const client = (): AxiosInstance => {
-  return axios.create({ baseURL: API_BASE_URL });
-};
