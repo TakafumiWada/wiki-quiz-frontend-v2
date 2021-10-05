78ee8f8b8028fae08d3cf61c718e7c0ae37dfcfe
WadaTakafumi <wdtk2525@gmail.com>
2021-10-05 14:59:40 +0900

    FIX: domain の変更

--- src/domain/Article.ts
+++
@@ -1,9 +0,0 @@
-export type Article = {
-  categories: Array<string>;
-  contents: Object;
-  title: string;
-  topic: Object;
-  words: Array<string>;
-  image: string;
-  url: string;
-};
---
+++ src/domain/Question.ts
@@ -0,0 +1,7 @@
+export type Question = {
+  title: string;
+  categories: Array<string>;
+  words: Array<string>;
+  image: string;
+  url: string;
+};
