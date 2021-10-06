10337e96bf6fce49cf7b1d899490d71c8997b39c
WadaTakafumi <wdtk2525@gmail.com>
2021-10-06 18:46:39 +0900

    FIX: 本番環境のAPIに接続

--- src/api/base.ts
+++ src/api/base.ts
@@ -18,7 +18,7 @@ import { Configuration } from "./configuration";
 // @ts-ignore
 import globalAxios, { AxiosPromise, AxiosInstance } from 'axios';
 
-export const BASE_PATH = "https://wiki-quiz-backend-dev.an.r.appspot.com/v2".replace(/\/+$/, "");
+export const BASE_PATH = "https://wiki-quiz-backend-prod.an.r.appspot.com/v2".replace(/\/+$/, "");
 
 /**
  *
