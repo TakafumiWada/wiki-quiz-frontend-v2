5bb528c58b574edac023300e1902af2e5395fd9b
WadaTakafumi <wdtk2525@gmail.com>
2021-10-07 11:34:59 +0900

    ADD: useHintの型を追加

--- src/composable/useHint.ts
+++ src/composable/useHint.ts
@@ -1,7 +1,13 @@
 import { ComputedRef, computed } from "vue";
 import { Question } from "@/types/Question";
 
-export const useHint = (question: ComputedRef<Question>) => {
+export interface Hint {
+  titleLength: ComputedRef<number>;
+  titleHead: ComputedRef<string>;
+  categories: ComputedRef<string[]>;
+}
+
+export const useHint = (question: ComputedRef<Question>): Hint => {
   return {
     titleLength: computed(() => question.value.title.length),
     titleHead: computed(() => question.value.title.charAt(0)),
