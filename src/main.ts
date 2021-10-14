44c3389d86e6552ac969abc687753f976c56fa7f
WadaTakafumi <wdtk2525@gmail.com>
2021-10-14 11:48:40 +0900

    ADD: vue-gtagを導入

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,15 @@
 import { createApp } from "vue";
+import VueGtag from "vue-gtag";
 import App from "./App.vue";
 import router from "./router";
 import store, { key } from "./store";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-createApp(App).use(store, key).use(router).mount("#app");
+createApp(App)
+  .use(store, key)
+  .use(router)
+  .use(VueGtag, {
+    config: { id: process.env.VUE_APP_GA_MEASUREMENT_ID },
+    router,
+  })
+  .mount("#app");
